8a8e3fbcc5f476f55730d888b3d43267d7e61419
Tam O'Donnell <thomashodonnell@gmail.com>
2021-02-07 16:28:30 +0000

    add comparitive settings

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const corsHeaders = require('./middleware/cors-headers');
 
 const app = express();
-const port = 80;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
@@ -11,7 +11,9 @@ app.use(bodyParser.json({ extended: true }));
 app.use(corsHeaders);
 
 app.get('/', (request, response) => {
-  response.json({ info: 'This is a CORS Test' });
+  response.json({
+    info: 'This is a CORS Test, you have successfully hit the API',
+  });
 });
 
 app.listen(port, () => {
--- middleware/cors-headers.js
+++ middleware/cors-headers.js
@@ -1,11 +1,17 @@
 module.exports = function (rea, res, next) {
+  // // Open Configuration (good for public APIs)
   // res.setHeader('Access-Control-Allow-Origin', '*');
 
   // res.setHeader('Access-Control-Allow-Methods', '*');
 
   // res.setHeader('Access-Control-Allow-Headers', '*');
 
-  // res.setHeader('Access-Control-Allow-Credentials', true);
+  // // Closed Configuration (should be default set up)
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   next();
 };
