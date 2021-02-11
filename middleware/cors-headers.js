b934281a74a420e0a2ad5a1ad2b93c989e7dba96
Tam O'Donnell <thomashodonnell@gmail.com>
2021-02-11 08:55:21 +0000

    update default headers

--- middleware/cors-headers.js
+++ middleware/cors-headers.js
@@ -1,15 +1,15 @@
 module.exports = function (rea, res, next) {
-  // Open Configuration (good for public APIs)
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // // Open Configuration (good for public APIs)
+  // res.setHeader('Access-Control-Allow-Origin', '*');
 
-  res.setHeader('Access-Control-Allow-Methods', '*');
+  // res.setHeader('Access-Control-Allow-Methods', '*');
 
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  // res.setHeader('Access-Control-Allow-Headers', '*');
 
   // // Closed Configuration (should be default set up)
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-  // res.setHeader('Access-Control-Allow-Methods', 'GET');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
